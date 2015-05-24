31a347f71254b87158089a634c5536d318d99f78
Bill Ma <qingyuma@design.upenn.edu>
2015-05-24 16:53:20 -0400

    Add image

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,5 @@ Rails.application.configure do
   # Required for Heroku
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+  Paperclip.options[:command_path] = 'D:\Softwares\GnuWin32\bin'
 end
---
+++ db/migrate/20150524070111_add_attachment_image_to_pins.rb
@@ -0,0 +1,10 @@
+class AddAttachmentImageToPins < ActiveRecord::Migration
+	def self.upto
+		change_table :pins do |t|
+			t.attachment :image
+		end
+	end
+	def self.down
+		drop_attached_file :pins, :image
+	end
+end
